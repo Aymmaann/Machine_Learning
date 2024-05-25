fe1c34b542a67327e5e63abc100617a7b7df4d72
Ayman Haseeb <114000374+Aymmaann@users.noreply.github.com>
2024-05-25 10:48:06 +0530

    Update snapchat.py

--- Snapchat Filter/snapchat.py
+++ Snapchat Filter/snapchat.py
@@ -5,20 +5,20 @@ import random
 import os
 
 
-folder_name = "/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Modified_Images/"
+folder_name = "./Modified_Images/"   # or specify your absolute path
 if not os.path.exists(folder_name):
    os.makedirs(folder_name)
 
 
-image_url = '/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Train/Jamie_Before.jpg'
-# image_url = '/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Test/Before.png'
+image_url = '/Train/Jamie_Before.jpg'
+# image_url = '/Test/Before.png'
 image = cv2.imread(image_url)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-eye_cascade = cv2.CascadeClassifier("/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Train/third-party/frontalEyes35x16.xml")  
+eye_cascade = cv2.CascadeClassifier("/Train/third-party/frontalEyes35x16.xml")  
 eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))                                  
-sunglasses = cv2.imread('/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Train/glasses.png', cv2.IMREAD_UNCHANGED)
+sunglasses = cv2.imread('/Train/glasses.png', cv2.IMREAD_UNCHANGED)
 for (x, y, w, h) in eyes:
   resized_sunglasses = cv2.resize(sunglasses, (w, h))
   x_offset = x
@@ -32,9 +32,9 @@ for (x, y, w, h) in eyes:
     image[y_offset:y_end, x_offset:x_end, c] = (alpha_s * resized_sunglasses[:, :, c] + alpha_l * image[y_offset:y_end, x_offset:x_end, c])
 
 
-nose_cascade = cv2.CascadeClassifier("/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Train/third-party/Nose18x15.xml")  
+nose_cascade = cv2.CascadeClassifier("/Train/third-party/Nose18x15.xml")  
 nose  = nose_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=13, minSize=(30, 30))
-mustache = cv2.imread("/Users/ayman/Desktop/AI_mafia/Snapchat Assignment/Train/mustache.png", cv2.IMREAD_UNCHANGED)
+mustache = cv2.imread("/Train/mustache.png", cv2.IMREAD_UNCHANGED)
 for (x, y, w, h) in nose:
     resized_mustache = cv2.resize(mustache, (int(w * 1.3), int(h * 1)))
     x_offset = x - int(w * 0.1)

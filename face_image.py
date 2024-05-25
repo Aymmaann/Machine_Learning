f61a3f88964bb919b81386fde2cd02aa5aab1a68
Ayman Haseeb <114000374+Aymmaann@users.noreply.github.com>
2024-05-25 10:53:18 +0530

    Delete face_image.py

--- face_image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-face_detection = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-image = cv2.imread('test.png')
-gray_scaling = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-faces = face_detection.detectMultiScale(gray_scaling, 1.1, 4)
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x,y), (x+h, y+w), (0,255,0), 2)
-    cv2.imshow('image', image) cv2.waitKey()
